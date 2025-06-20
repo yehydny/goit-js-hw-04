977fbfde0237341dc522d64ed1208ea45ec55641
Dmytro Osadchuk <144940208+yehydny@users.noreply.github.com>
2025-06-20 18:36:53 +0300

    solve tasks

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+
+    changeUsername(newName) {
+        profile.username = newName;
+    },
+
+    updatePlayTime(hours) {
+        profile.playTime += hours;
+    },
+
+    getInfo() {
+        return `${profile.username} has ${profile.playTime} active hours!`;
+    },
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
+
